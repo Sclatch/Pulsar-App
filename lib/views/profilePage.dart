07bf5cbeefe221a0faa47eeac080a830d9d8d760
HaydenTLM <hayden.murray1@uoit.net>
2020-11-12 22:49:55 -0500

    Fixed Profile Page
    Fixed the profile page

--- lib/views/profilePage.dart
+++ lib/views/profilePage.dart
@@ -7,7 +7,6 @@ import 'package:Pulsar/model/posts.dart';
 import 'package:Pulsar/model/postsModel.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
 class ProfilePage extends StatefulWidget {
   ProfilePage({Key key, this.title, this.user}) : super(key: key);
   final User user;
@@ -23,13 +22,12 @@ class _ProfilePageState extends State<ProfilePage> {
   String description;
 
   _ProfilePageState({this.user}) {
-    if(user!=null) {
-      username=user.username;
-      description=user.description;
-    } 
-    else {
-      username="Anonymous";
-      description="Anonymous User";
+    if (user != null) {
+      username = user.username;
+      description = user.description;
+    } else {
+      username = "Anonymous";
+      description = "Anonymous User";
     }
 
     print(username + "'s profile has been opened");
@@ -37,33 +35,7 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    PostsModel postsModel;
-    if(user!=null) {
-      FutureBuilder(
-        //This is how you search for a user
-        future: postsModel.searchPost(username),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            List posts = snapshot.data.docs;
-              return ListView.builder(
-                itemCount: posts.length,
-                itemBuilder: (BuildContext context, int index) {
-                  //This is just to show the indexed post
-                  DocumentSnapshot postDocument = posts[index];
-
-                  //This takes a post from the database and makes it an instance of post
-                  final post = Post.fromMap(postDocument.data(),
-                      reference: postDocument.reference);
-                  print(post);
-
-                  return pulseCard(context, post, user);
-                });
-          }
-          else {
-            return Center( child: CircularProgressIndicator(),);
-        }
-      });
-    }
+    PostsModel postsModel = PostsModel();
 
     return Scaffold(
       floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
@@ -73,7 +45,10 @@ class _ProfilePageState extends State<ProfilePage> {
         onPressed: () {
           Navigator.pop(context);
         },
-        child: Icon(Icons.keyboard_backspace, color: Colors.white,),
+        child: Icon(
+          Icons.keyboard_backspace,
+          color: Colors.white,
+        ),
       ),
       body: Column(
         children: <Widget>[
@@ -82,40 +57,49 @@ class _ProfilePageState extends State<ProfilePage> {
             height: 175,
             color: Colors.lightBlue,
             child: Center(
-              child: CircleAvatar(
-                radius: 50,
-                backgroundColor: Colors.blueGrey
-              )
-            ),
+                child:
+                    CircleAvatar(radius: 50, backgroundColor: Colors.blueGrey)),
           ),
           SizedBox(height: 10),
           Text(username,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 35
-            )
-          ),
+              textAlign: TextAlign.center, style: TextStyle(fontSize: 35)),
           Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-            height: 100,
-            child: Text(description,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 17
-            ),
-            )
-          ),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              height: 100,
+              child: Text(
+                description,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 17),
+              )),
           Container(
-            padding: const EdgeInsets.symmetric(horizontal: 5),
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * 0.60,
-            child: ListView.builder(
-              itemCount: 4,
-              itemBuilder: (BuildContext context, int index) {
-                return pulseCard(context, Post(), user);
-              },
-            )
-          )
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.60,
+              child: FutureBuilder(
+                  //This is how you search for a user
+                  future: postsModel.searchPostUser(user.username),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      List posts = snapshot.data.docs;
+                      return ListView.builder(
+                          itemCount: posts.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            //This is just to show the indexed post
+                            DocumentSnapshot postDocument = posts[index];
+
+                            //This takes a post from the database and makes it an instance of post
+                            final post = Post.fromMap(postDocument.data(),
+                                reference: postDocument.reference);
+                            print(post);
+
+                            return pulseCard(context, post, user);
+                          });
+                    } else {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  }))
         ],
       ),
     );
